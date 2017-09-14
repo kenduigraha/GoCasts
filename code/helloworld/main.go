5ab903707b70f541f657948de539a34a81e27089
kenduigraha <ken.duigraha@kudo.co.id>
2017-09-14 19:45:53 +0700

    hello world go

--- code/helloworld/main.go
+++ code/helloworld/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi there!")
+	fmt.Print("Hello World fmt.Print()")
+	fmt.Println("Hello World")
+	fmt.Printf("Hello World fmt.Printf()")
+
+	fmt.Print(1)
+	fmt.Println(2)
+	// fmt.Printf(3) -> fmt.Printf must not a integer
+	fmt.Printf("3")
+
+	// NB : use double quotes not a single quotes
 }
