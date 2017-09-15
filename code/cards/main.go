cff4446bb2954099ac7e942e636a97a92e7ad6c7
kenduigraha <ken.duigraha@kudo.co.id>
2017-09-15 14:31:44 +0700

    test variable

--- code/cards/main.go
+++ code/cards/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "King"
+	// var card string = "King"
+	card := "Card"
+	card = "Kartu"
+
+	// test := 1 // it can string
+	// fmt.Println(test)
+
 	fmt.Println(card)
 }
