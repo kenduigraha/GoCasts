ed38c10b7146e65b5f900806c0e62120a565965c
kenduigraha <ken.duigraha@kudo.co.id>
2017-09-15 14:34:20 +0700

    global variable

--- code/cards/main.go
+++ code/cards/main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+var card string = "Card"
+
 func main() {
 	// var card string = "King"
-	card := "Card"
-	card = "Kartu"
+	// card := "Card"
+	// card = "Kartu"
 
 	// test := 1 // it can string
 	// fmt.Println(test)
